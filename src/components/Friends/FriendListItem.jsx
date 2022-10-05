549bbde7f2b235e5fbe35395be9fb9bf8515f929
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-10-05 19:42:43 +0300

    fix bags in styles

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -2,20 +2,12 @@ import PropTypes from 'prop-types';
 import s from '../Friends/Friends.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const color = isOnline ? s.online : s.offline;
+  const newStyle = [color, s.status].join(' ');
+
   return (
     <li className={s.item}>
-      <span
-        style={{
-          display: 'block',
-          width: '20px',
-          height: '20px',
-          borderRadius: '50%',
-          backgroundColor: isOnline ? 'green' : 'red',
-        }}
-        className={s.status}
-      >
-        {isOnline}
-      </span>
+      <span className={newStyle}>{isOnline}</span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </li>
